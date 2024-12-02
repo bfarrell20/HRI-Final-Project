500754a28a8eba598e698e4ee4a2681ed014d0ce
Russell Abedeen <43918499+russellabedeen@users.noreply.github.com>
2024-12-02 00:28:24 -0500

    Update Algo_Skeleton.js
    tested it out a bit, the main problem is the WAITING_FOR_COLLISION state

--- Algo_Skeleton.js
+++ Algo_Skeleton.js
@@ -1,3 +1,10 @@
+/*
+ * changes needed: 
+ * change distance to an integer instead of a float
+ * figure out better behavior for the waiting for collision state...it's hard to prevent it from hanging
+ * it reads out underscores, so maybe we shouldn't have it announce the state has changed in the final product
+*/
+
 // Define states
 const STATES = {
     WAITING_FOR_COLLISION: 'WAITING_FOR_COLLISION',
@@ -5,9 +12,17 @@ const STATES = {
     MOVING_BACKWARD: 'MOVING_BACKWARD',
     TERMINAL: 'TERMINAL',
   };
+const STATE_COLORS = {
+	WAITING_FOR_COLLISION: { r: 102, g: 255, b: 255 }, //blue
+	MOVING_FORWARD: { r: 90, g: 255, b: 90 }, //green
+	MOVING_BACKWARD: { r: 102, g: 255, b: 255 }, //yellow
+	TERMINAL: { r: 255, g: 102, b: 102 }, //red
+};
   
   let currentState = STATES.WAITING_FOR_COLLISION;
   let startPosition = null;
+  let first_distance = 0;
+  let final_distance = 0;
   
   
   // Function to change state and update LED color
@@ -26,6 +41,8 @@ const STATES = {
         await changeState(STATES.MOVING_FORWARD);
         break;
       case STATES.MOVING_FORWARD:
+        final_distance = await getDistance();
+		await spin(180, 1);
         await changeState(STATES.MOVING_BACKWARD);
         break;
       case STATES.MOVING_BACKWARD:
@@ -40,18 +57,31 @@ const STATES = {
       switch (currentState) {
         case STATES.WAITING_FOR_COLLISION:
             //store starting position
+          first_distance = await getDistance();
+          await delay(5);
+		  await changeState(STATES.MOVING_FORWARD);
           break;
   
         case STATES.MOVING_FORWARD:
-          await roll(0, 50, 2); // Roll forward
+          await roll(0, 50, 20); // Roll forward
           break;
   
         case STATES.MOVING_BACKWARD:
             //Move back to user and get distance traveled
+          final_distance = await getDistance();
+          await roll(0, 50, 20);
           await changeState(STATES.TERMINAL);
           break;
   
         case STATES.TERMINAL:
+          if (final_distance == 0) {
+              await speak("No obstacles detected.");
+          }
+          else {
+              distance = final_distance - first_distance;
+              text = "Obstacle detected at " + distance.toString() + " centimeters.";
+              await speak(text);
+          }
           await speak("Program ended.");
           await exitProgram();
           break;
